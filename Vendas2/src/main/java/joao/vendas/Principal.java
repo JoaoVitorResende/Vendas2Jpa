0a44fdb889c89d24786a0cf7ed4163f53cef6364
João Vitor Resende <joaovitorresende.jr@gmail.com>
2022-08-10 20:18:05 -0300

    introdução a banco de dados com spring

--- Vendas2/src/main/java/joao/vendas/Principal.java
+++ Vendas2/src/main/java/joao/vendas/Principal.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Principal {
 
     public static void main(String[] args) {
-        SpringApplication.run(VendaApplication.class, args);
+        SpringApplication.run(Principal.class, args);
     }
 
 }
